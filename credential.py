73a93ca59a03eb9d565c18c6a7b57a759bba114f
Munge Ryan <mungeryan@gmail.com>
2020-10-11 17:32:05 +0300

    class method to display credentials

--- credential.py
+++ credential.py
@@ -1,11 +1,10 @@
 class Creds:
     creds_list = []
 
-    def __init__(self, snapchat, instagram, twitter, E_mail):
+    def __init__(self, snapchat, instagram, twitter):
         self.instagram = instagram
         self.snapchat = snapchat
         self.twitter = twitter
-        self.E_mail = E_mail
 
     def save_creds(self):
         Creds.creds_list.append(self)
