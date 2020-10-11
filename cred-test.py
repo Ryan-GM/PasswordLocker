1fcd79417bac38f7fc4a793ea9ae4960a7deb553
Munge Ryan <mungeryan@gmail.com>
2020-10-11 17:33:08 +0300

    if condition to test the application

--- cred-test.py
+++ cred-test.py
@@ -3,13 +3,12 @@ from credential import Creds
 
 class TestCreds(unittest.TestCase):
     def setUp(self):
-        self.new_creds = Creds("rg-munge", "ryan.dfw", "rm-githanji", "ryanmunge12@gmail.com")
+        self.new_creds = Creds("rg-munge", "ryan.dfw", "rm-githanji")
 
     def test_init(self):
         self.assertEqual(self.new_creds.snapchat,"rg-munge")
         self.assertEqual(self.new_creds.twitter,"rm-githanji")
         self.assertEqual(self.new_creds.instagram,"ryan.dfw")
-        self.assertEqual(self.new_creds.E_mail,"ryanmunge12@gmail.com")
 
     def test_save_info(self):
         self.new_creds.save_creds()
@@ -21,7 +20,7 @@ class TestCreds(unittest.TestCase):
 
     def test_delete_creds(self):
         self.new_creds.save_creds()
-        test_creds = Creds("ethansolo", "s-ethan", "scottethan", "ethanbaraka@gmail.com")
+        test_creds = Creds("ethansolo", "s-ethan", "scottethan")
         test_creds.save_creds()
         test_creds.delete_creds()
         self.assertEqual(len(Creds.creds_list),1)
