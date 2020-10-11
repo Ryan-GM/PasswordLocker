7e0317eb3d199481b3eb2f1126da532677c477c9
Munge Ryan <mungeryan@gmail.com>
2020-10-11 11:44:46 +0300

    testing if user can be removed

--- user-test.py
+++ user-test.py
@@ -5,10 +5,19 @@ class TestUser(unittest.TestCase):
     def setUp(self):
         self.new_user = User("Ryan","Munge","ryanmunge12@gmail.com")
 
-    def test1(self):
+    def test_init(self):
         self.assertEqual(self.new_user.F_name,"Ryan")
         self.assertEqual(self.new_user.L_name,"Munge")
         self.assertEqual(self.new_user.E_mail,"ryanmunge12@gmail.com")
 
     def tearDown(self):
         User.userList = []
+
+    def test_save_user(self):
+        self.new_user.save-user()
+
+    def test_delete_user():
+        self.new_user.save_user()
+        test_data = User("Scott","Ethan","ethanbaraka@gmail.com")
+        test_data.save_user()
+        self.assertEqual(len(User.user_list),2)
