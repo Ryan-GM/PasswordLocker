b44cc0f50a2b05f88428b028a6c119e845c6eddb
Munge Ryan <mungeryan@gmail.com>
2020-10-11 12:15:30 +0300

    class decorator to display credentials

--- credential.py
+++ credential.py
@@ -6,3 +6,13 @@ class Creds:
         self.snapchat = snapchat
         self.twitter = twitter
         self.E_mail = E_mail
+
+    def save_creds(self):
+        Creds.creds_list.append(self)
+
+    def delete_creds(self):
+        Creds.creds_list.remove(self)
+
+    @classmethod
+    def display_info(cls):
+        return cls.info_list
