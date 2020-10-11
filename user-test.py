ef8125a010587c655ee88b313136ad7f91f00421
Munge Ryan <mungeryan@gmail.com>
2020-10-11 11:37:42 +0300

    defining a tearDown method to clean after ran tests

--- user-test.py
+++ user-test.py
@@ -5,4 +5,10 @@ class TestUser(unittest.TestCase):
     def setUp(self):
         self.new_user = User("Ryan","Munge","ryanmunge12@gmail.com")
 
-    
+    def test1(self):
+        self.assertEqual(self.new_user.F_name,"Ryan")
+        self.assertEqual(self.new_user.L_name,"Munge")
+        self.assertEqual(self.new_user.E_mail,"ryanmunge12@gmail.com")
+
+    def tearDown(self):
+        User.userList = []
