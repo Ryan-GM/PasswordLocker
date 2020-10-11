7a9eaeaa6a2a448ebfb075e461e87a72fe4c5aee
Munge Ryan <mungeryan@gmail.com>
2020-10-11 12:11:25 +0300

    defining properties of credentials

--- credential.py
+++ credential.py
@@ -1,2 +1,8 @@
 class Creds:
     creds_list = []
+
+    def __init__(self,snapchat, instagram, twitter, E_mail):
+        self.instagram = instagram
+        self.snapchat = snapchat
+        self.twitter = twitter
+        self.E_mail = E_mail
